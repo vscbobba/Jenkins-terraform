683474208b3fe95f0a9496986b934279f356a5ee
chowdarybvsn <chowdarybvsn@gmail.com>
2024-03-06 16:58:16 +0530

    yashed in output.tf

--- output.tf
+++ output.tf
@@ -1,12 +1,12 @@
 output "aws_jenkins" {
     value = aws_instance.lab_Jenkins.public_ip
 }
-output "aws_ansible_manager" {
-    value = aws_instance.lab_ansible_manger.public_ip
-}
-output "aws_ansible_node1" {
-    value = aws_instance.lab_ansible_node1.public_ip
-}
-output "aws_ansible_node2" {
-    value = aws_instance.lab_ansible_node2.public_ip
-}
+# output "aws_ansible_manager" {
+#     value = aws_instance.lab_ansible_manger.public_ip
+# }
+# output "aws_ansible_node1" {
+#     value = aws_instance.lab_ansible_node1.public_ip
+# }
+# output "aws_ansible_node2" {
+#     value = aws_instance.lab_ansible_node2.public_ip
+# }
