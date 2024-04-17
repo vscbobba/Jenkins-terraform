7dafc2a1292bdb9c964e7c18dac8cfd3726b3e8d
vscbobba <59333292+vscbobba@users.noreply.github.com>
2024-04-17 20:25:26 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,7 +6,7 @@ terraform {
   }
   backend "s3" {
    bucket = "terraform-project-1-2024"
-   key = "state/terraform.tfstate"
+   key = "EKS1/terraform.tfstate"
    region = "us-east-1"
    encrypt = true
    dynamodb_table = "2024"
