a234d7c86b22249eeef9a7db70491eb8b40c18fb
vscbobba <59333292+vscbobba@users.noreply.github.com>
2024-04-17 16:11:11 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,6 +4,13 @@ terraform {
         source = "hashicorp/aws"
     }
   }
+  backend "s3" {
+   bucket = "terraform-project-1-2024"
+   key = "state/terraform.tfstate"
+   region = "us-east-1"
+   encrypt = true
+   dynamodb_table = "2024"
+  }
 }
 
 provider "aws" {
